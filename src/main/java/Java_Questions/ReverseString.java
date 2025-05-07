6edc3c301f00599925c09da0b0a31640930a3d7e
Praveen <praveen@4axissolutions.com>
2025-05-07 17:33:52 +0530

    RevereseString

---
+++ src/main/java/Java_Questions/ReverseString.java
@@ -0,0 +1,25 @@
+package Java_Questions;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+//        // Using CharAt() method to reverse the string
+//
+        String str = "Hello World";
+//        int len = str.length();
+//
+//        for (int i = len - 1; i >= 0; i--) {
+//            System.out.print(str.charAt(i));
+//        }
+
+        // Using toCharArray() method to reverse the string
+
+        char[] ch = str.toCharArray();
+        int l = ch.length;
+
+        for (int i = l - 1; i >= 0; i--) {
+            System.out.print(ch[i]);
+        }
+    }
+}
